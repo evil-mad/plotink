e361df3373b87274c5c63035f388342ddf77dd5e
Windell Oskay <windell@oskay.net>
2018-04-16 03:49:29 -0700

    plot_utils v.10
    Add two new functions:
    pathdata_first_point
    pathdata_last_point
    
    These parse the path data string (the text of the 'd' attribute of an SVG path) and return the first or last point from that data.

--- libraries/plot_utils.py
+++ libraries/plot_utils.py
@@ -6,12 +6,12 @@
 # Intended to provide some common interfaces that can be used by 
 # EggBot, WaterColorBot, AxiDraw, and similar machines.
 #
-# Version 0.9.0, Dated October 15, 2017.
+# Version 0.10.0, Dated April 16, 2018.
 #
 #
 # The MIT License (MIT)
 # 
-# Copyright (c) 2017 Windell H. Oskay, Evil Mad Scientist Laboratories
+# Copyright (c) 2018 Windell H. Oskay, Evil Mad Scientist Laboratories
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -38,7 +38,7 @@ from bezmisc import beziersplitatt
 
 
 def version():
-    return "0.9"  # Version number for this document
+    return "0.10"  # Version number for this document
 
 
 PX_PER_INCH = 90.0  # 90 px per inch, as of Inkscape 0.91
@@ -184,10 +184,10 @@ def parseLengthWithUnits(string_to_parse):
     elif s[-2:] == 'cm':  # centimeters
         s = s[:-2]
         u = 'cm'
-    elif s[-2:] == 'pt':  # points	1pt = 1/72th of 1in
+    elif s[-2:] == 'pt':  # points; 1pt = 1/72th of 1in
         s = s[:-2]
         u = 'pt'
-    elif s[-2:] == 'pc':  # picas!	1pc = 1/6th of 1in
+    elif s[-2:] == 'pc':  # picas; 1pc = 1/6th of 1in
         s = s[:-2]
         u = 'pc'
     elif s[-1:] == 'Q' or s[-1:] == 'q':  # quarter-millimeters. 1q = 1/40th of 1cm
@@ -346,3 +346,253 @@ def vFinal_Vi_A_Dx(v_initial, acceleration, delta_x):
         return sqrt(final_v_squared)
     else:
         return -1
+
+
+def pathdata_first_point(path):
+    '''
+    Return the first (X,Y) point from an SVG path data string
+    
+    Input:  A path data string; the text of the 'd' attribute of an SVG path
+    Output: Two floats in a list representing the x and y coordinates of the first point
+    '''
+    
+    # Path origin's default values are used to see if we have
+    # Written anything to the path_origin variable yet
+    MaxLength = len(path)
+    ix = 0
+    tempString = ''
+    x_val = ''
+    y_val = ''
+    # Check one char at a time
+    # until we have the moveTo Command
+    while ix < MaxLength:
+        if path[ix].upper() == 'M':
+            break
+        # Increment until we have M
+        ix = ix + 1
+
+    # Parse path until we reach a digit, decimal point or negative sign
+    while ix < MaxLength:
+        if(path[ix].isdigit()) or path[ix] == '.' or path[ix] == '-':
+            break
+        ix = ix + 1
+
+    # Add digits and decimal points to x_val
+    # Stop parsing when next character is neither a digit nor a decimal point
+    while ix < MaxLength:
+        if  (path[ix].isdigit()):
+            tempString = tempString + path[ix]
+            x_val = float(tempString )
+            ix = ix + 1
+        # If next character is a decimal place, save the decimal and continue parsing
+        # This allows for paths without leading zeros to be parsed correctly
+        elif (path[ix] == '.' or path[ix] == '-'):
+            tempString = tempString + path[ix]
+            ix = ix + 1
+        else:
+            ix = ix + 1
+            break
+
+    # Reset tempString for y coordinate
+    tempString = ''
+    
+    # Parse path until we reach a digit or decimal point
+    while ix < MaxLength:
+        if(path[ix].isdigit()) or path[ix] == '.' or path[ix] == '-':
+            break
+            ix = ix + 1
+    
+    # Add digits and decimal points to y_val
+    # Stop parsin when next character is neither a digit nor a decimal point
+    while ix < MaxLength:
+        if (path[ix].isdigit() ):
+            tempString = tempString + path[ix]
+            y_val = float(tempString)
+            ix = ix + 1
+        # If next character is a decimal place, save the decimal and continue parsing
+        # This allows for paths without leading zeros to be parsed correctly
+        elif (path[ix] == '.' or path[ix] == '-'):
+            tempString = tempString + path[ix]
+            ix = ix + 1
+        else:
+            ix = ix + 1
+            break
+    return [x_val,y_val]
+
+
+def pathdata_last_point(path):
+    '''
+    Return the last (X,Y) point from an SVG path data string
+    
+    Input:  A path data string; the text of the 'd' attribute of an SVG path
+    Output: Two floats in a list representing the x and y coordinates of the last point
+    
+    SVG path data can be formatted in a great many ways
+    This uses certain tricks to avoid deep parsing of the entire string,
+    but will parse the entire string if necessary.
+    
+    Strings can be very large, and can be formatted such that there are good
+    hints about the position (absolute moves, for example) or only 
+    relative data, which would require parsing the entire string from the start.
+    '''
+    
+    # If a path ends with a z command, the final position is the first position
+    if path[-1].upper() == 'Z':
+        return pathdata_first_point(path)
+
+    # If there is no z or absolute commands, we need to parse the entire path 
+    else:
+        # scan path for absolute coordinates. If present, begin parsing from their index
+        # instead of the beginning
+        jx = 0
+        x_val, y_val = 0,0    
+        # Check one char at a time 
+        # until we have the moveTo Command
+        last_command = ''
+        path_length = len(path)
+        is_absolute_command = False
+        repeated_command = False
+        # name of command
+        # how many parameters we need to skip 
+        accepted_commands = {
+            'M':0,
+            'L':0,
+            'H':0,
+            'V':0,
+            'C':4,
+            'S':2,
+            'Q':2,
+            'T':0,
+            'A':5
+        }    
+        
+        # If there is an absolute command which specifies a new initial point 
+        # then we can save time by setting our index directly to its location in the path data
+        # See if an accepted_command is present in the path data. If it is present further in the 
+        # string than any command found before, then set the pointer to that location 
+        # if a command is not found, find() will return a -1. jx is initialized to 0, so if no matches
+        # are found, the program will parse from the beginning to the end of the path
+        
+        for keys in 'MLCSQTA':
+            if(path.find(keys) > jx):
+                jx = path.find(keys)        
+
+        while jx < path_length:
+
+            temp_x_val = ''
+            temp_y_val = ''
+            num_of_params_to_skip = 0
+            
+            # SVG Path commands can be repeated 
+            if (path[jx].isdigit() and last_command):
+                repeated_command = True    
+            else:
+                repeated_command = False
+
+            if (path[jx].isalpha() and path[jx].upper() in accepted_commands) or repeated_command:
+                if repeated_command:
+                    #is_relative_command is saved from last iteration of the loop
+                    current_command = last_command
+                else:
+                    # If the character is accepted, we must parse until reach the x y coordinates
+                    is_absolute_command = path[jx].isupper()    
+                    current_command = path[jx].upper()
+
+                # Each command has a certain number of parameters we must pass before we reach the
+                # information we care about. We will parse until we know that we have reached them
+
+                # Get to start of next number
+                # We will know we have reached a number if the current character is a +/- sign
+                # or current character is a digit 
+                while jx < path_length:
+                    if(path[jx] in '+-' or path[jx].isdigit()):
+                        break
+                    jx = jx + 1 
+                        
+                # We need to parse past the unused parameters in our command
+                # The number of parameters to parse past is dependent on the command and stored 
+                # as the value of accepted_command
+                # Spaces and commas are used to deliniate paramters 
+                while jx < path_length and num_of_params_to_skip < accepted_commands[current_command]:
+                    if(path[jx].isspace() or path[jx] == ','):
+                        num_of_params_to_skip = num_of_params_to_skip + 1 
+                    jx = jx + 1 
+
+                # Now, we are in front of the x character
+
+                if current_command.upper() == 'V':
+                    temp_x_val = 0    
+                if current_command.upper() == 'H':
+                    temp_y_val = 0    
+
+                # Parse until next character is a digit or +/- character
+                while jx < path_length and current_command.upper() != 'V':
+                    if(path[jx] in '+-' or path[jx].isdigit()):
+                        break
+                    jx = jx + 1 
+                
+                # Save each next character until we reach a space
+                while jx < path_length and current_command.upper() != 'V' and not (path[jx].isspace() or path[jx] == ','):
+                    temp_x_val = temp_x_val + path[jx]
+                    jx = jx + 1 
+                
+                # Then we know we have completely parsed the x character
+
+                # Now we are in front of the y character
+                # Parse until next character is a digit or +/- character
+                while jx < path_length and current_command.upper() != 'H':
+                    if(path[jx] in '+-' or path[jx].isdigit()):
+                        break
+                    jx = jx + 1 
+
+                # Save each next character until we reach a space
+                while jx < path_length and current_command.upper() != 'H' and not (path[jx].isspace() or path[jx] == ','):
+                    temp_y_val = temp_y_val + path[jx]
+                    jx = jx + 1 
+                
+                # Then we know we have completely parsed the y character
+
+                if is_absolute_command:
+
+                    if current_command == 'H':
+                        # Absolute commands create new x,y position 
+                        try:
+                            x_val = float(temp_x_val)
+                        except ValueError:
+                            pass
+                    elif current_command == 'V':
+                        # Absolute commands create new x,y position 
+                        try:
+                            y_val = float(temp_y_val)
+                        except ValueError:
+                            pass
+                    else:
+                        # Absolute commands create new x,y position 
+                        try:
+                            x_val = float(temp_x_val)
+                            y_val = float(temp_y_val)
+                        except ValueError:
+                            pass
+                else:
+                    if current_command == 'h':
+                        # Absolute commands create new x,y position 
+                        try:
+                            x_val = x_val + float(temp_x_val)
+                        except ValueError:
+                            pass
+                    elif current_command == 'V':
+                        # Absolute commands create new x,y position 
+                        try:
+                            y_val = y_val + float(temp_y_val)
+                        except ValueError:
+                            pass
+                    else:
+                        # Absolute commands create new x,y position 
+                        try:
+                            x_val = x_val + float(temp_x_val)
+                            y_val = y_val + float(temp_y_val)
+                        except ValueError:
+                            pass
+                last_command = current_command
+            jx = jx + 1
+        return [x_val,y_val]
