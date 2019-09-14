a75a5cf286df60c6d5f434ca99b8ea5fcbbaf7d5
Shoshana Berleant <sberleant@gmail.com>
2019-09-14 16:14:19 -0400

    squash

--- libraries/plot_utils_import.py
+++ libraries/plot_utils_import.py
@@ -6,6 +6,7 @@ import os
 
 DEPENDENCY_DIR_NAME = 'axidraw_deps'
 DEPENDENCY_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), DEPENDENCY_DIR_NAME)
+INK_EXTENSIONS_DIR = os.path.join(DEPENDENCY_DIR, 'ink_extensions')
 
 def from_dependency_import(module_name):
     ''' module_name ex: "ink_extensions", "ink_extensions.inkex"
@@ -20,12 +21,14 @@ def from_dependency_import(module_name):
         if 'ink_extensions' in module_name:
             # a special case: inkscape-provided files don't know they are
             # in the ink_extensions module
-            sys.path.insert(0, os.path.join(DEPENDENCY_DIR, 'ink_extensions'))
+            sys.path.insert(0, INK_EXTENSIONS_DIR)
 
         try:
             module = import_module(module_name)
         finally:
-            sys.path[0] = old_path
+            for folder in [DEPENDENCY_DIR, INK_EXTENSIONS_DIR]:
+                if folder in sys.path:
+                    sys.path.remove(folder)
 
     else:
         # running as a python module with traditionally installed packages
