c6409879ab1018b084e5080ea85b85fa2f6ecf36
Windell Oskay <windell@oskay.net>
2026-03-08 00:24:14 -0500

    Version bumps

--- plotink/ebb3_serial.py
+++ plotink/ebb3_serial.py
@@ -34,7 +34,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
 
-__version__ = '0.2.2'  # Dated 2026-03-07
+__version__ = '0.2.3'  # Dated 2026-03-08
 
 from packaging.version import parse, InvalidVersion
 
